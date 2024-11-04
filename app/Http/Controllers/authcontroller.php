208443efb6d3a5964d29af578ab9ac161392e3fd
RaisulAp <raisulagung@gmail.com>
2024-11-04 22:04:39 +0700

    first commit

---
+++ app/Http/Controllers/authcontroller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class authcontroller extends Controller
+{
+    function redirect(){
+        return Socialite::driver('google')->redirect();
+
+    } 
+
+    function callback(){
+        $user = Socialite::driver('google')->user();
+        $id = $user->id;
+        $email = $user->email;
+        $name = $user->name;
+
+        $user = User::updateOrCreate(
+            ['email'=>$email],
+            [
+                'name'=> $name,
+                'google_id' => $id
+            ],
+        
+        );
+
+    }
+}
